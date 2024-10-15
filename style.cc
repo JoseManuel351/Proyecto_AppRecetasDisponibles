3a8c1536926aa93fcdb64090cea2ce501254a08e
Leo-Herrera0303 <21460528@colima.tecnm.mx>
2024-10-15 10:32:06 -0600

    Create style.css

---
+++ style.cc
@@ -0,0 +1,60 @@
+body {
+    margin: 0;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #CAD2C5;
+}
+form {
+    background-color: white;
+    width: 350px;
+    height:500px;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+form label, form input[type="text"], form input[type="password"], form input[type="submit"] {
+    display: block;
+    margin-bottom: 10px;
+
+  
+}
+
+#cabeceralogo > div{
+
+    margin: 0px auto;
+
+    text-align: center;
+}
+.form-item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 10px;
+  }
+  
+  .form-item label {
+    margin-bottom: 5px;
+  }
+  .button-center {
+    display: block;
+    margin: 0 auto;
+  }
+  #titulo > div{
+    margin: 0px auto;
+
+    text-align: center;
+  }
+  .imagen-container {
+    position: absolute;
+    top: 0;
+    left: 0;
+}
+.imagen-container2 {
+  position:absolute;
+  top: 0;
+  right: 0;
+}
+
+
